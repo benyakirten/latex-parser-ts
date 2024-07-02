508730b7f0c9dec96d44b734ca67f910fc3933d4
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-07-02 12:50:44 +0200

    Change author.test to properly interpolate variables [ci skip]

--- src/ast/fonts/commands/author.test.ts
+++ src/ast/fonts/commands/author.test.ts
@@ -5,108 +5,45 @@ import { LatexFontShape, LatexFontSizeUnit, LatexFontWeight, LatexFontWidth } fr
 
 describe("parseAuthorCommand", () => {
   test.each([
-    {
-      want: null,
-      input: "unknown",
-    },
-    {
-      want: { shape: LatexFontShape.Normal, width: LatexFontWidth.Medium, family: "prefers-serif" },
-      input: "textnormal",
-    },
-    {
-      want: { shape: LatexFontShape.Normal, width: LatexFontWidth.Medium, family: "prefers-serif" },
-      input: "normalfont",
-    },
-    {
-      want: { family: "prefers-serif" },
-      input: "textrm",
-    },
-    {
-      want: { family: "prefers-serif" },
-      input: "rmfamily",
-    },
-    {
-      want: { family: "prefers-sans" },
-      input: "textsf",
-    },
-    {
-      want: { family: "prefers-sans" },
-      input: "sffamily",
-    },
-    {
-      want: { family: "prefers-monospace" },
-      input: "texttt",
-    },
-    {
-      want: { family: "prefers-monospace" },
-      input: "ttfamily",
-    },
-    {
-      want: { weight: LatexFontWeight.Medium, width: LatexFontWidth.Medium },
-      input: "textmd",
-    },
-    {
-      want: { weight: LatexFontWeight.Medium, width: LatexFontWidth.Medium },
-      input: "mdseries",
-    },
-    {
-      want: { weight: LatexFontWeight.Bold, width: LatexFontWidth.Expanded },
-      input: "textbf",
-    },
-    {
-      want: { weight: LatexFontWeight.Bold, width: LatexFontWidth.Expanded },
-      input: "bfseries",
-    },
-    {
-      want: { shape: LatexFontShape.Italic },
-      input: "textit",
-    },
-    {
-      want: { shape: LatexFontShape.Italic },
-      input: "itshape",
-    },
-    {
-      want: { shape: LatexFontShape.Oblique },
-      input: "textsl",
-    },
-    {
-      want: { shape: LatexFontShape.Oblique },
-      input: "slshape",
-    },
-    {
-      want: { shape: LatexFontShape.Swash },
-      input: "textsw",
-    },
-    {
-      want: { shape: LatexFontShape.Swash },
-      input: "swshape",
-    },
-    {
-      want: null,
-      input: "textulc",
-    },
-    {
-      want: null,
-      input: "ulcshape",
-    },
-    {
-      want: null,
-      input: "textup",
-    },
-    {
-      want: null,
-      input: "upshape",
-    },
-    { input: "tiny", want: { size: { value: 5, unit: LatexFontSizeUnit.Point } } },
-    { input: "scriptsize", want: { size: { value: 7, unit: LatexFontSizeUnit.Point } } },
-    { input: "footnotesize", want: { size: { value: 8, unit: LatexFontSizeUnit.Point } } },
-    { input: "normalsize", want: { size: { value: 10, unit: LatexFontSizeUnit.Point } } },
-    { input: "large", want: { size: { value: 12, unit: LatexFontSizeUnit.Point } } },
-    { input: "Large", want: { size: { value: 14.4, unit: LatexFontSizeUnit.Point } } },
-    { input: "LARGE", want: { size: { value: 17.28, unit: LatexFontSizeUnit.Point } } },
-    { input: "huge", want: { size: { value: 20.74, unit: LatexFontSizeUnit.Point } } },
-    { input: "Huge", want: { size: { value: 24.88, unit: LatexFontSizeUnit.Point } } },
-  ])("should return $want given $input", ({ want, input }) => {
+    [null, "unknown"],
+    [
+      { shape: LatexFontShape.Normal, width: LatexFontWidth.Medium, family: "prefers-serif" },
+      "textnormal",
+    ],
+    [
+      { shape: LatexFontShape.Normal, width: LatexFontWidth.Medium, family: "prefers-serif" },
+      "normalfont",
+    ],
+    [{ family: "prefers-serif" }, "textrm"],
+    [{ family: "prefers-serif" }, "rmfamily"],
+    [{ family: "prefers-sans" }, "textsf"],
+    [{ family: "prefers-sans" }, "sffamily"],
+    [{ family: "prefers-monospace" }, "texttt"],
+    [{ family: "prefers-monospace" }, "ttfamily"],
+    [{ weight: LatexFontWeight.Medium, width: LatexFontWidth.Medium }, "textmd"],
+    [{ weight: LatexFontWeight.Medium, width: LatexFontWidth.Medium }, "mdseries"],
+    [{ weight: LatexFontWeight.Bold, width: LatexFontWidth.Expanded }, "textbf"],
+    [{ weight: LatexFontWeight.Bold, width: LatexFontWidth.Expanded }, "bfseries"],
+    [{ shape: LatexFontShape.Italic }, "textit"],
+    [{ shape: LatexFontShape.Italic }, "itshape"],
+    [{ shape: LatexFontShape.Oblique }, "textsl"],
+    [{ shape: LatexFontShape.Oblique }, "slshape"],
+    [{ shape: LatexFontShape.Swash }, "textsw"],
+    [{ shape: LatexFontShape.Swash }, "swshape"],
+    [null, "textulc"],
+    [null, "ulcshape"],
+    [null, "textup"],
+    [null, "upshape"],
+    [{ size: { value: 5, unit: LatexFontSizeUnit.Point } }, "tiny"],
+    [{ size: { value: 7, unit: LatexFontSizeUnit.Point } }, "scriptsize"],
+    [{ size: { value: 8, unit: LatexFontSizeUnit.Point } }, "footnotesize"],
+    [{ size: { value: 10, unit: LatexFontSizeUnit.Point } }, "normalsize"],
+    [{ size: { value: 12, unit: LatexFontSizeUnit.Point } }, "large"],
+    [{ size: { value: 14.4, unit: LatexFontSizeUnit.Point } }, "Large"],
+    [{ size: { value: 17.28, unit: LatexFontSizeUnit.Point } }, "LARGE"],
+    [{ size: { value: 20.74, unit: LatexFontSizeUnit.Point } }, "huge"],
+    [{ size: { value: 24.88, unit: LatexFontSizeUnit.Point } }, "Huge"],
+  ])("should return %o given %s", (want, input) => {
     const got = parseAuthorCommand(input);
     expect(got).toEqual(want);
   });
