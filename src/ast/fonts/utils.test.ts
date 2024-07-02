2919cfc2141400637fe81c4c078227e1d9060baa
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-07-02 16:44:59 +0200

    Add parse font encoding tests [ci skip]

--- src/ast/fonts/utils.test.ts
+++ src/ast/fonts/utils.test.ts
@@ -1,28 +1,36 @@
 import { expect, describe, test, it } from "bun:test";
 
 import { parseFontEncoding, parseFontSeries, parseFontShape, parseFontMeasurement } from "./utils";
-import type {
-  LatexFontEncoding,
-  LatexFontMeasurement,
-  LatexFontSeries,
-  LatexFontShape,
+import {
+  LatexFontEncodingNormalValue,
+  LatexFontEncodingType,
+  type LatexFontMeasurement,
+  type LatexFontSeries,
+  type LatexFontShape,
 } from "./types";
 
-describe.todo("parseFontEncoding", () => {
-  test.each<[LatexFontEncoding, string]>([])(
-    "should return a normal encoding of %p for an input of %s",
-    (want, input) => {
-      const got = parseFontEncoding(input);
-      expect(got).toEqual(want);
-    },
-  );
+describe("parseFontEncoding", () => {
+  test.each<[LatexFontEncodingNormalValue, string]>([
+    [LatexFontEncodingNormalValue.KnuthTexText, "OT1"],
+    [LatexFontEncodingNormalValue.ExtendedText, "T1"],
+    [LatexFontEncodingNormalValue.MathItalic, "OML"],
+    [LatexFontEncodingNormalValue.MathLargeSymbols, "OMX"],
+    [LatexFontEncodingNormalValue.MathSymbols, "OMS"],
+    [LatexFontEncodingNormalValue.Unknown, "U"],
+  ])("should return a normal encoding of %p for an input of %s", (want, input) => {
+    const got = parseFontEncoding(input);
+    expect(got.type).toEqual(LatexFontEncodingType.Normal);
+    expect(got.encoding).toEqual(want);
+  });
 
   it("should give a local encoding based on input if the first letter is L", () => {
-    // TODO
+    const got = parseFontEncoding("LT1");
+    expect(got.type).toEqual(LatexFontEncodingType.Local);
+    expect(got.encoding).toEqual("LT1");
   });
 
   it("should throw for all other values", () => {
-    // TODO
+    expect(() => parseFontEncoding("unknown")).toThrow();
   });
 });
 
