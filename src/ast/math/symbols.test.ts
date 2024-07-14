7d13bf1b43df1b18699244d4b6b176777d24daf7
Benyakir Horowitz <benyakir.horowitz@gmail.com>
2024-07-14 17:12:25 +0200

    Add tests for declareMathAccent [ci skip]

--- src/ast/math/symbols.test.ts
+++ src/ast/math/symbols.test.ts
@@ -1,6 +1,10 @@
 import { expect, it, describe, test } from "bun:test";
 
-import { declareMathSymbol, getMathSymbolType } from "./symbols";
+import {
+	declareMathAccent,
+	declareMathSymbol,
+	getMathSymbolType,
+} from "./symbols";
 import { MathSymbolFont, MathSymbolType, MathSymbolValueType } from "./types";
 import {
 	LatexCommandArgumentType,
@@ -458,6 +462,386 @@ describe("declareMathSymbol", () => {
 		};
 		const symbolFonts = ["symbolFont"];
 		const got = declareMathSymbol(command, symbolFonts);
+		expect(got).toEqual({
+			symbol: { type: MathSymbolValueType.Char, value: "-" },
+			type: MathSymbolType.Ordinary,
+			fontSlot: { symbolFont: "symbolFont", slot: '"00' },
+		});
+	});
+});
+
+describe("declareMathAccent", () => {
+	it("should return null if command name is not 'DeclareMathAccent'", () => {
+		const command: CommandToken = {
+			name: "InvalidCommand",
+			literal: "\\InvalidCommand",
+			type: LatexTokenType.Command,
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "-",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "0",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: MathSymbolFont.Operators,
+							originalLength: MathSymbolFont.Operators.length,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+					],
+				},
+			],
+		};
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return null if number of arguments is not 4", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+			arguments: [],
+		};
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return null if any of arguments is not required", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "-",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "0",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: MathSymbolFont.Operators,
+							originalLength: MathSymbolFont.Operators.length,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Optional,
+					content: {
+						type: LatexTokenType.Content,
+						literal: '"00',
+						originalLength: 3,
+					},
+				},
+			],
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+		};
+
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return null if any argument's content has length !== 1", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "-",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "0",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: MathSymbolFont.Operators,
+							originalLength: MathSymbolFont.Operators.length,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+					],
+				},
+			],
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+		};
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return null if name token is not a valid math symbol name", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "0",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: MathSymbolFont.Operators,
+							originalLength: MathSymbolFont.Operators.length,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+					],
+				},
+			],
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+		};
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return null if type token is not a valid math symbol type", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "-",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "2000",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: MathSymbolFont.Operators,
+							originalLength: MathSymbolFont.Operators.length,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+					],
+				},
+			],
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+		};
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return null if symbol font or slot is not valid", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "-",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "0",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "invalid",
+							originalLength: 7,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+					],
+				},
+			],
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+		};
+		const got = declareMathAccent(command);
+		expect(got).toBeNull();
+	});
+
+	it("should return a valid MathSymbol object if all conditions are met", () => {
+		const command: CommandToken = {
+			name: "DeclareMathAccent",
+			arguments: [
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "-",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "0",
+							originalLength: 1,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: "symbolFont",
+							originalLength: 10,
+						},
+					],
+				},
+				{
+					type: LatexCommandArgumentType.Required,
+					content: [
+						{
+							type: LatexTokenType.Content,
+							literal: '"00',
+							originalLength: 1,
+						},
+					],
+				},
+			],
+			literal: "\\DeclareMathAccent",
+			type: LatexTokenType.Command,
+		};
+		const symbolFonts = ["symbolFont"];
+		const got = declareMathAccent(command, symbolFonts);
 		expect(got).toEqual({
 			symbol: { type: MathSymbolValueType.Char, value: "-" },
 			type: MathSymbolType.Ordinary,
